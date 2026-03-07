7c3a13c7a8f133ed616e82f3b0f78a1cefece451
Marco Stroppel <marco.stroppel@tapio.one>
2026-03-07 09:30:50 +0100

    feat: solution created with worker service and docker file

---
+++ src/Rafatz.CalendarSync.Worker/Worker.cs
@@ -0,0 +1,14 @@
+namespace Rafatz.CalendarSync;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
+            
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
